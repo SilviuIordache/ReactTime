a40a5b4f74ed7e30dca3c4abbe0effb997f8272a
Silviu Iordache <silviu.iordache92@gmail.com>
2021-11-22 22:46:44 +0200

    Remove nl

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { NavLink } from "react-router-dom";
 import './Navigation.css';
 
-
 export default class Navigation extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
     };
   }
-
 
   render() {
     return (
